b057ae8704d4c7fd98df20a35696bf6b6bba056d
benli <ben.li@thoughtworks.com>
2020-06-04 22:05:36 +0800

    add micro.css and change dir when build es

--- postcss.config.js
+++ postcss.config.js
@@ -1,5 +1,6 @@
 module.exports = {
     plugins: [
+        require('postcss-import')({ path: ['./lib'] }),
         require('postcss-preset-env'),
         require('autoprefixer'),
         require('postcss-nested'),
